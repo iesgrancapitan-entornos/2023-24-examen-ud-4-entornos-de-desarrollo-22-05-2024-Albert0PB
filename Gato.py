a90b898f005c2a7e4745c596391ab6da0954fc57
Albert0PB <a23pebeal@iesgrancapitan.org>
2024-05-22 09:33:56 +0200

    Documentacion 1 - Alberto Pérez Bernabeu

--- Gato.py
+++ Gato.py
@@ -3,8 +3,15 @@ Clase Gato.
 :author: Jaime Rabasco Ronda.
 """
 class Gato:
+    """
+    Implementación de una clase 'Gato'
+    """
 
     def maullar(self):
+        """
+        Método de los objetos de esta clase que hace que el Gato maúlle
+        :return:
+        """
         self.maulla = 'Miau'
         print('%s' % self.maulla);
 
